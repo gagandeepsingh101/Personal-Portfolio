5d3b98d44bc07e028074e149d6fc7fea4ab319b5
gagandeepsingh101 <gagandeepsingh128548@gmail.com>
2023-10-25 20:15:26 +0530

    Create homepage of portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Navigation Toggle
+
+let navToggle = $(".nav-toggle");
+let navList = $(".nav-list");
+navToggle.on("click", function () {
+  navList.toggleClass("show-nav");
+  navToggle.toggleClass("active");
+});
+
+// Typewriter text
+
+
