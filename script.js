5298fcf3584189d38c0df450db2ba052bb467863
gagandeepsingh101 <gagandeepsingh128548@gmail.com>
2023-10-30 13:48:37 +0530

    Add active nav and scroll top on reload feature

--- script.js
+++ script.js
@@ -7,8 +7,50 @@ navToggle.on("click", function () {
   navToggle.toggleClass("active");
 });
 
+// Hide navigation on click on nav item in mobile screen
+$(document).ready(function () {
+  var allTabs = $(".nav-list li a");
+  allTabs.on("click", function () {
+    $(".nav-list").toggleClass("show-nav");
+    $(".nav-toggle").toggleClass("active");
+  });
+});
+
 // Smooth scrolling
 $(document).ready(function () {
+  // set active state of navigation nav link according to section on screen
+  $(window).scroll(function () {
+    let scrollPos = $(window).scrollTop() + 1;
+    activeTab("home", scrollPos);
+    activeTab("about", scrollPos);
+    activeTab("skill", scrollPos);
+    activeTab("project", scrollPos);
+    activeTab("contact", scrollPos);
+  });
+
+  // in these function we set activeTab acccroding to its top position and height of current Section
+  function activeTab(currentSec, scrollPos) {
+    let currentSecTopPos = $(`.${currentSec}`).offset().top - 200;
+    let currentSecHeight = $(`.${currentSec}`).height();
+    if (
+      scrollPos >= currentSecTopPos &&
+      scrollPos < currentSecTopPos + currentSecHeight
+    ) {
+      $(`.${currentSec}-link`).css("color", "#FFD15C");
+    } else {
+      $(`.${currentSec}-link`).css("color", "white");
+    }
+  }
+
+  // goes website to top on reloading
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    1000
+  );
+
+  // on click navigation scroll to desired section
   $("a").on("click", function (event) {
     if (this.hash !== "") {
       event.preventDefault();
@@ -27,27 +69,34 @@ $(document).ready(function () {
 });
 
 // move to other section of page on some button click
-function scrollToElement(element,target) {
+function scrollToElement(element, target) {
   $(document).ready(function () {
     $(element).on("click", function (event) {
-      $("html, body").animate({
-        scrollTop:$(target).offset().top
-      },500);
+      $("html, body").animate(
+        {
+          scrollTop: $(target).offset().top,
+        },
+        500
+      );
     });
   });
 }
 
-scrollToElement("#hire-btn",".contact");
-scrollToElement("#scroll-animation",".about");
+scrollToElement("#hire-btn", ".contact");
+scrollToElement("#scroll-animation", ".about");
 
 // sending message
 let form = $(".form-sec form");
+
+// fetch form data
 form.submit(function (e) {
   e.preventDefault();
   let name = $("#name").val();
   let email = $("#email-address").val();
   let subject = $("#subject").val();
   let message = $("#message").val();
+
+  // add validation on form submit
   if (name && email && subject && message) {
     sendMessage(name, email, subject, message);
   } else {
@@ -75,7 +124,7 @@ function sendMessage(name, email, subject, message) {
     Subject: `${subject}`,
     Body: `<h5>Name : ${name}<br>
     Email : ${email}</h5> <br>${message}`,
-  }).then(()=>{
+  }).then(() => {
     visibleMessage("Message sent successfully", "success");
   });
 }
@@ -84,11 +133,10 @@ function sendMessage(name, email, subject, message) {
 function visibleMessage(message, res) {
   $(".response").toggleClass("show-response");
   $(".response").text(message);
-  if (res==="success") {
+  if (res === "success") {
     $(".response").removeClass("error");
     $(".response").addClass("success");
-  }
-  else{
+  } else {
     $(".response").removeClass("success");
     $(".response").addClass("error");
   }
@@ -96,3 +144,6 @@ function visibleMessage(message, res) {
     $(".response").toggleClass("show-response");
   }, 1000);
 }
+
+
+// 
